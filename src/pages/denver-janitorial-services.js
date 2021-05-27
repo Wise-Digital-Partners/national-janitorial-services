0fc350026a3cc2132c49a0a4c69cc75263bc0ccb
Hamlet <hamletavetisian@gmail.com>
2021-05-26 18:58:32 -0700

    Update button mobile text color

--- src/pages/denver-janitorial-services.js
+++ src/pages/denver-janitorial-services.js
@@ -42,7 +42,7 @@ const Page = ({ data }) => {
             <p className="text-xl md:text-3xl text-accent mb-7 md:mb-10">Keeping workspaces clean, employees safe, and your mind at peace.</p>
             <div className="grid  gap-y-6 md:flex md:items-center">
                <ButtonSolid as="button" modal="modal-contact" text="Get a Free Estimate" className="md:mr-6" />
-               <ButtonGhost href="tel:+1-424-260-7369" text="(424) 260-7369" />
+               <ButtonGhost className="text-white md:text-primary" href="tel:+1-424-260-7369" text="(424) 260-7369" />
             </div>
          </HeroFullWidth>
 
