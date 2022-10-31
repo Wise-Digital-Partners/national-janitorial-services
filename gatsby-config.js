341ba3a0a20e984bf4225b5c039ce3aca1790316
Bas <sebastian@wisedigitalpartners.com>
2022-10-31 15:38:15 -0400

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 const {
-   NODE_ENV,
-   URL: NETLIFY_SITE_URL = "https://www.nationaljanitorialservices.com/",
-   DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
-   CONTEXT: NETLIFY_ENV = NODE_ENV,
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = "https://longbeachjanitorial.com/",
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
 } = process.env;
 const isNetlifyProduction = NETLIFY_ENV === "production";
 const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
